dd418e8bf8eb719b426ea1d4d301d67515cae535
Guilherme henrique machado <progr.guilherme@gmail.com>
2021-02-04 05:22:11 -0300

    fix: statusbar android

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, FlatList, StatusBar, Platform, SafeAreaView } from 'react-native';
+import { colors } from './src/Constants';
 import Home from './src/Containers/Home'
 
 export default function App() {
   return (
     <SafeAreaView>
+      <StatusBar backgroundColor={colors.primary} barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"} />
       <Home />
     </SafeAreaView>
   );
