3c0f3dded1e73d9ef8b5d9c6670de4a36453c62f
Guilherme Henrique Machado <progr.guilherme@gmail.com>
2021-02-04 14:44:32 -0300

    feat: navigation and intro screen

--- src/Components/SeeMore/styles.ts
+++ src/Components/SeeMore/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native'
-import { sizes } from '../../Constants';
+import { sizes, colors } from '../../Constants';
 
 interface ITransactionItem {
     paddingTop: boolean;
@@ -9,8 +9,9 @@ export const SeeMoreContainer = styled.TouchableOpacity<ITransactionItem>`
     width: 100%;
     flex-direction: row;
     justify-content: flex-end;
-    padding-horizontal: ${sizes.space.large};
-    padding-vertical: ${sizes.space.medium};
+    background-color: ${colors.white};
+    padding-horizontal: ${sizes.space.large}px;
+    padding-vertical: ${sizes.space.medium}px;
     margin-right: ${(props) => (props.paddingTop ? `${sizes.space.medium}px` : 0)};
 
 `;
